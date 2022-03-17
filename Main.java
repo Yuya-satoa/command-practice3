808792a81233a122f931c3192a768d8319bae35c
Yuya Sato <yuuya.satou.sys21@morijyobi.ac.jp>
2022-03-17 12:28:31 +0900

    main.javaの新規作成

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner  x = new Scanner(System.in);
+
+        System.out.print(" height(m) add");
+    double height = x.nextDouble();
+        System.out.print(" weight(Kg) add");
+    double weight = x.nextDouble();
+
+    double bmi = weight/Math.pow(height,2);
+ 
+    System.out.println("BMI is" + bmi );
+
+    }
+}
